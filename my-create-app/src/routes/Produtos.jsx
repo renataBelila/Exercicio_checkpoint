e8341ec19f473db6270effa3198d39eea4b9c16d
renataBelila <renata.belila1@gmail.com>
2023-09-05 21:56:57 -0300

    exibindo produtos na tela

--- my-create-app/src/routes/Produtos.jsx
+++ my-create-app/src/routes/Produtos.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom"
+import { listaProdutos } from "../components/listaProdutos"
+
 export default function Produtos(){
     return(
         <>
             <main>
                 <h1>Produtos</h1>
+                {listaProdutos.map(prod => (
+                    <div key={prod.id}> 
+                        <Link to={`/produtos/editar/${prod.id}`}> 
+                            Editar o Produto: {prod.nome}
+                        </Link>
+                    </div>
+
+                ))}
             </main>
         </>
     )
