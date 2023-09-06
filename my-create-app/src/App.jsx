8e1158b72e15918cf8dd838f895d86d1f09102dd
renataBelila <renata.belila1@gmail.com>
2023-09-05 21:35:28 -0300

    Colocando o rodapé

--- my-create-app/src/App.jsx
+++ my-create-app/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Menu from './components/Menu'
+import Rodape from './components/Rodape'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
     <Menu/>
     <Outlet/>
+    <Rodape/>
     </>
   )
 }
